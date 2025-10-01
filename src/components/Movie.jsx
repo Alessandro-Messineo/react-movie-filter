69238e9fd83c35a86535bd7f7dc02be321bcf493
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-10-01 18:30:50 +0200

    bonus ricerca per titolo

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -14,35 +14,55 @@ export default function Movie() {
     const [movies] = useState(arrayMovie);
     // stato del array filtrato
     const [filteredMovies, setFilteredMovies] = useState(movies);
+    // stato del campo di selezione
+    const [select, setSelect] = useState("");
     // stato del campo di ricerca
     const [search, setSearch] = useState("");
 
-     // utilizzo useEffect per gestione filtro
+    // utilizzo useEffect per gestione filtro
     useEffect(() => {
-        if (search === "") {
+        if (select === "") {
             setFilteredMovies(movies);
         } else {
             setFilteredMovies(
                 movies.filter(movie =>
-                    movie.genre.toLowerCase().includes(search.toLowerCase().trim())
+                    movie.genre.toLowerCase().includes(select.toLowerCase().trim())
+
+                    // altro modo per fare il filtraggio
+                    // movie.genre === select
                 )
             );
         }
+    }, [select, movies]);
+
+    useEffect(() => {
+        setFilteredMovies(
+            movies.filter(movie => {
+                return movie.title.toLowerCase().includes(search.toLowerCase().trim())
+            })
+        );
     }, [search, movies]);
 
 
     return (
         <>
-         <h1>Filtra per genere:</h1>
-         {/* selezione del genere */}
-            <select name="movies" id="movies" onChange={(e) => { setSearch(e.target.value) }}>
+            <h1>Filtra per genere:</h1>
+            {/* selezione per genere */}
+            <select name="movies" id="movies" onChange={(e) => { setSelect(e.target.value) }}>
                 <option value="" >Tutti</option>
                 <option value="Fantascienza" >Fantascienza</option>
                 <option value="Thriller" >Thriller</option>
                 <option value="Romantico" >Romantico</option>
                 <option value="Azione" >Azione</option>
-              
+
             </select>
+            <h2>Filtra per titolo:</h2>
+            {/* selezione per titolo */}
+            <input type="text"
+                placeholder='Cerca'
+                value={search}
+                onChange={(e) => { setSearch(e.target.value) }}
+            />
             <h2>Risultati:</h2>
             {/* liste dei film filtrati */}
             <ul>
